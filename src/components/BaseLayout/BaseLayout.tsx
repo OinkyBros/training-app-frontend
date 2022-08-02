8667c2ae2d6ade2b95ddcd44b4dbac5fb2930a90
Ploinky <mail@johannes-litger.de>
2022-08-02 20:25:00 +0200

    Initial commit

---
+++ src/components/BaseLayout/BaseLayout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from '../Header/Header';
+
+function BaseLayout() {
+    return (
+        <div>
+            <Header />
+            <main>MAIN</main>
+            <footer>FOOTER</footer>
+        </div>
+    )
+}
+
+export default BaseLayout;
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BaseLayout from '../../components/BaseLayout/BaseLayout';
+
+function HomePage() {
+    return (
+        <BaseLayout />
+    )
+}
+
+export default HomePage;
