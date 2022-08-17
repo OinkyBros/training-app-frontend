d4c8521664bf6f3e19357cda64c6795c02c2351f
Johannes Litger <mail@johannes-litger.de>
2022-08-17 23:05:33 +0200

    Sort players according to role

--- src/services/Matches.ts
+++ src/services/Matches.ts
@@ -3,6 +3,15 @@ import Role from '../types/Role';
 import MatchMode from '../types/MatchMode';
 import Match from '../types/Match';
 
+const actionOrder = {
+  [Role.TOP]: 0,
+  [Role.JUNGLE]: 1,
+  [Role.MID]: 2,
+  [Role.BOT]: 3,
+  [Role.SUPP]: 4,
+  [Role.NONE]: 5,
+};
+
 async function getMatches(): Promise<Match[]> {
   const url: string = "https://api.oinky.vhoeher.de/api/v1/matches";
 
@@ -12,13 +21,13 @@ async function getMatches(): Promise<Match[]> {
     
   }).then((response) => response.json())
     .then((data: Match[]) => {
-      data.forEach((m: any) => {
+      data.forEach((m: Match) => {
         m.Timestamp *= 1000;
+        m.Teams[0].Participants.sort((a, b) => actionOrder[a.Role] - actionOrder[b.Role]);
+        m.Teams[1].Participants.sort((a, b) => actionOrder[a.Role] - actionOrder[b.Role]);
       });
       return data;
     });
-
-  console.log(response);
 
   return response ?? [];
 }
@@ -32,8 +41,10 @@ async function getMatch(matchID: string): Promise<Match> {
     mode: 'cors',
     
   }).then((response) => response.json())
-  .then((data) => {
+  .then((data: Match) => {
     data.Timestamp *= 1000;
+    data.Teams[0].Participants.sort((a, b) => actionOrder[a.Role] - actionOrder[b.Role]);
+    data.Teams[1].Participants.sort((a, b) => actionOrder[a.Role] - actionOrder[b.Role]);
     return data;
   });
 
