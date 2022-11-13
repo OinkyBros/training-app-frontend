09f58c2800355defba88e4e8af3f06fc989e4b4c
Ploinky <mail@johannes-litger.de>
2022-11-13 17:27:19 +0100

    add additional score cards

--- src/components/Lan/ScoreOverview/ScoreCard.tsx
+++ src/components/Lan/ScoreOverview/ScoreCard.tsx
@@ -4,16 +4,19 @@ import styles from './ScoreCard.module.scss';
 interface ScoreCardProps {
     title: string;
     p1: string;
+    p1Value: number;
     p2: string;
+    p2Value: number;
     p3: string;
+    p3Value: number;
 }
 
-function ScoreCard({ title, p1, p2, p3 }: ScoreCardProps) {
+function ScoreCard({ title, p1, p2, p3, p1Value, p2Value, p3Value }: ScoreCardProps) {
     return (
         <div className={styles.card}>
-            <h3>{p1}</h3>
-            <h4>{p2}</h4>
-            <h5>{p3}</h5>
+            <h3>{p1}: {p1Value.toFixed(2).replace('.00', '')}</h3>
+            <h4>{p2}: {p2Value.toFixed(2).replace('.00', '')}</h4>
+            <h5>{p3}: {p3Value.toFixed(2).replace('.00', '')}</h5>
             <h1>{title}</h1>
         </div>
     )    
--- src/components/Lan/ScoreOverview/ScoreOverview.tsx
+++ src/components/Lan/ScoreOverview/ScoreOverview.tsx
@@ -9,12 +9,18 @@ import styles from './ScoreOverview.module.scss';
 type PlayerScore = {
   summonerName: string,
   totalCS: number,
+  averageCS: number,
   totalKills: number,
+  averageKills: number;
   totalAssists: number;
+  averageAssists: number;
   totalDeaths: number;
+  averageDeaths: number;
+  matches: number;
 }
 
 function ScoreOverview() {
+  const [loading, setLoading] = useState<boolean>(true);
 	const [matches, setMatches] = useState<Match[]>([]);
 
 	const matchElements: React.ReactElement[] = [];
@@ -31,25 +37,43 @@ function ScoreOverview() {
     }
   )}, []);
 
+  useEffect(() => {
+    setLoading(matches.length <= 0);
+  }, [matches]);
+
   const players = matches.flatMap((m) => m.Teams).flatMap((t) => t.Participants).filter((p) => p.IsOinky);
   const playerScores: Map<string, PlayerScore> = new Map();
 
   players.forEach((p) => {
     const summ = playerScores.get(p.SummonerName) ?? {
       summonerName: p.SummonerName,
-      totalKills: p.Kills,
-      totalAssists: p.Assists,
-      totalDeaths: p.Deaths,
-      totalCS: p.CS,
+      totalKills: 0,
+      averageKills: 0,
+      totalAssists: 0,
+      averageAssists: 0,
+      totalDeaths: 0,
+      averageDeaths: 0,
+      totalCS: 0,
+      averageCS: 0,
+      matches: 0,
     } as PlayerScore;
 
     summ.totalCS += p.CS;
     summ.totalKills += p.Kills;
     summ.totalAssists += p.Assists;
     summ.totalDeaths += p.Deaths;
+    summ.matches += 1;
 
     playerScores.set(p.SummonerName, summ);
   });
+
+  playerScores.forEach((p, key) => {
+    p.averageKills = p.totalKills / p.matches;
+    p.averageAssists = p.totalAssists / p.matches;
+    p.averageDeaths = p.totalDeaths / p.matches;
+    p.averageCS = p.totalCS / p.matches;
+    playerScores.set(key, p);
+  })
 
   const scoreItem = (children: ReactNode) => (
     <GridItem xs={12} sm={6} md={4} lg={3} xl={2}>
@@ -63,7 +87,15 @@ function ScoreOverview() {
     const ks = [...playerScores.values()].sort((a, b) => b.totalKills - a.totalKills);
 
     return (
-      <ScoreCard title="Total Kills" p1={ks.at(0)?.summonerName ?? ''} p2={ks.at(1)?.summonerName ?? ''} p3={ks.at(2)?.summonerName ?? ''} />
+      <ScoreCard
+        title="Total Kills"
+        p1={ks.at(0)?.summonerName ?? ''}
+        p1Value={ks.at(0)?.totalKills ?? 0}
+        p2={ks.at(1)?.summonerName ?? ''}
+        p2Value={ks.at(1)?.totalKills ?? 0}
+        p3={ks.at(2)?.summonerName ?? ''}
+        p3Value={ks.at(2)?.totalKills ?? 0}
+      />
     );
   };
   
@@ -71,7 +103,15 @@ function ScoreOverview() {
     const ks = [...playerScores.values()].sort((a, b) => b.totalAssists - a.totalAssists);
 
     return (
-      <ScoreCard title="Total Assists" p1={ks.at(0)?.summonerName ?? ''} p2={ks.at(1)?.summonerName ?? ''} p3={ks.at(2)?.summonerName ?? ''} />
+      <ScoreCard
+        title="Total Assists"
+        p1={ks.at(0)?.summonerName ?? ''}
+        p1Value={ks.at(0)?.totalAssists ?? 0}
+        p2={ks.at(1)?.summonerName ?? ''}
+        p2Value={ks.at(1)?.totalAssists ?? 0}
+        p3={ks.at(2)?.summonerName ?? ''}
+        p3Value={ks.at(2)?.totalAssists ?? 0}
+      />
     );
   };
   
@@ -79,7 +119,15 @@ function ScoreOverview() {
     const ks = [...playerScores.values()].sort((a, b) => b.totalDeaths - a.totalDeaths);
 
     return (
-      <ScoreCard title="Total Deaths" p1={ks.at(0)?.summonerName ?? ''} p2={ks.at(1)?.summonerName ?? ''} p3={ks.at(2)?.summonerName ?? ''} />
+      <ScoreCard
+        title="Total Deaths"
+        p1={ks.at(0)?.summonerName ?? ''}
+        p1Value={ks.at(0)?.totalDeaths ?? 0}
+        p2={ks.at(1)?.summonerName ?? ''}
+        p2Value={ks.at(1)?.totalDeaths ?? 0}
+        p3={ks.at(2)?.summonerName ?? ''}
+        p3Value={ks.at(2)?.totalDeaths ?? 0}
+      />
     );
   };
   
@@ -87,17 +135,93 @@ function ScoreOverview() {
     const ks = [...playerScores.values()].sort((a, b) => b.totalCS - a.totalCS);
 
     return (
-      <ScoreCard title="Total CS" p1={ks.at(0)?.summonerName ?? ''} p2={ks.at(1)?.summonerName ?? ''} p3={ks.at(2)?.summonerName ?? ''} />
+      <ScoreCard
+        title="Total CS"
+        p1={ks.at(0)?.summonerName ?? ''}
+        p1Value={ks.at(0)?.totalCS ?? 0}
+        p2={ks.at(1)?.summonerName ?? ''}
+        p2Value={ks.at(1)?.totalCS ?? 0}
+        p3={ks.at(2)?.summonerName ?? ''}
+        p3Value={ks.at(2)?.totalCS ?? 0}
+      />
+    );
+  };
+  
+  const avKillCard = () => {
+    const ks = [...playerScores.values()].sort((a, b) => b.averageKills - a.averageKills);
+
+    return (
+      <ScoreCard
+        title="Kills/Game"
+        p1={ks.at(0)?.summonerName ?? ''}
+        p1Value={ks.at(0)?.averageKills ?? 0}
+        p2={ks.at(1)?.summonerName ?? ''}
+        p2Value={ks.at(1)?.averageKills ?? 0}
+        p3={ks.at(2)?.summonerName ?? ''}
+        p3Value={ks.at(2)?.averageKills ?? 0}
+      />
+    );
+  };
+  
+  const avAssistsCard = () => {
+    const ks = [...playerScores.values()].sort((a, b) => b.averageAssists - a.averageAssists);
+
+    return (
+      <ScoreCard
+        title="Assists/Game"
+        p1={ks.at(0)?.summonerName ?? ''}
+        p1Value={ks.at(0)?.averageAssists ?? 0}
+        p2={ks.at(1)?.summonerName ?? ''}
+        p2Value={ks.at(1)?.averageAssists ?? 0}
+        p3={ks.at(2)?.summonerName ?? ''}
+        p3Value={ks.at(2)?.averageAssists ?? 0}
+      />
+    );
+  };
+  
+  const avDeathsCard = () => {
+    const ks = [...playerScores.values()].sort((a, b) => b.averageDeaths - a.averageDeaths);
+
+    return (
+      <ScoreCard
+        title="Deaths/Game"
+        p1={ks.at(0)?.summonerName ?? ''}
+        p1Value={ks.at(0)?.averageDeaths ?? 0}
+        p2={ks.at(1)?.summonerName ?? ''}
+        p2Value={ks.at(1)?.averageDeaths ?? 0}
+        p3={ks.at(2)?.summonerName ?? ''}
+        p3Value={ks.at(2)?.averageDeaths ?? 0}
+      />
+    );
+  };
+  
+  const avCSCard = () => {
+    const ks = [...playerScores.values()].sort((a, b) => b.averageCS - a.averageCS);
+
+    return (
+      <ScoreCard
+        title="CS/Game"
+        p1={ks.at(0)?.summonerName ?? ''}
+        p1Value={ks.at(0)?.averageCS ?? 0}
+        p2={ks.at(1)?.summonerName ?? ''}
+        p2Value={ks.at(1)?.averageCS ?? 0}
+        p3={ks.at(2)?.summonerName ?? ''}
+        p3Value={ks.at(2)?.averageCS ?? 0}
+      />
     );
   };
 
-  return (
-      <Grid>
+  return loading ? <div className={styles.loadingScreen}>Loading</div> : (
+    <Grid>
         {scoreItem(killCard())}
         {scoreItem(assistCard())}
         {scoreItem(deathCard())}
         {scoreItem(csCard())}
-      </Grid>
+        {scoreItem(avKillCard())}
+        {scoreItem(avAssistsCard())}
+        {scoreItem(avDeathsCard())}
+        {scoreItem(avCSCard())}
+    </Grid>
   );
 }
 
