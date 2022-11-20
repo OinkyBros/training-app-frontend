1a13cbb3f82874885dd70a4a93859e68c5d5f9bd
Johannes Litger <mail@johannes-litger.de>
2022-11-20 12:56:14 +0100

    remove link to lan scores

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import HomePage from "./pages/HomePage/HomePage";
 import BaseLayout from './components/BaseLayout/BaseLayout';
 import './assets/index.scss';
 import MatchDetail from "./pages/MatchDetail/MatchDetail";
-import ScoreOverview from "./components/Lan/ScoreOverview/ScoreOverview";
 import Imprint from "./pages/Imprint/Imprint";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/lan" element={<ScoreOverview />} />
           <Route path="/imprint" element={<Imprint />} />
           <Route path="/matches/:matchID" element={<MatchDetail />} />
         </Routes>
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from '../../components/Button/Button';
 import PlayerLineup from '../../components/PlayerLineup/PlayerLineup';
-import GoalService from '../../services/Goals';
 import styles from './HomePage.module.scss';
 
 function HomePage() {
   return (
     <div className={styles.container}>
-      <Link to="/lan"><Button>Lan-Party scores</Button></Link>
       <PlayerLineup />
     </div>
   );
