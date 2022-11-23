59e286172a3c960809cb44a0baf0f90bec79bcc5
Johannes Litger <mail@johannes-litger.de>
2022-11-23 09:58:26 +0100

    champino icon as url, not blob

--- src/services/Icons.ts
+++ src/services/Icons.ts
@@ -4,26 +4,8 @@ class IconService {
     return `https://api.oinky.vhoeher.de/api/v1/icon/profile/${iconId}`;
   }
 
-  public static async getChampionIcon(championName: string): Promise<Blob | null> {
-    const url: string = `https://api.oinky.vhoeher.de/api/v1/icon/champion/${championName}`;
-  
-    const response = await fetch(url, {
-      method: 'GET',
-      mode: 'cors',
-    }).then((response) => response.blob());
-
-    return response ?? null;
-  }
-
-  public static async getPlayerIcon(iconId: number): Promise<Blob | null> {
-    const url: string = `https://api.oinky.vhoeher.de/api/v1/icon/profile/${iconId}`;
-  
-    const response = await fetch(url, {
-      method: 'GET',
-      mode: 'cors',
-    }).then((response) => response.blob());
-
-    return response ?? null;
+  public static async getChampionIconUrl(championName: string): Promise<string> {
+    return `https://api.oinky.vhoeher.de/api/v1/icon/champion/${championName}`;
   }
 };
 
